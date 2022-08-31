2d15a14bf9fc06a56866f34c969462c316b97d3b
FreshDev <billyhans90@gmail.com>
2022-08-31 21:15:49 +0100

    Add the index and create method in the controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,5 +7,20 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
 
+    public function create(Request $request)
+    {
+        $product = new Product;
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->description = $request->description;
+
+        $product->save();
+        return response()->json($product);
+    }
 }
